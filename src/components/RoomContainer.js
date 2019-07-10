21364a79e31aa33b85381eeb06cd1df97a03f0fd
qe <qe@qes-Mac-mini.local>
2019-07-10 11:26:24 +0700

    push 4

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,23 +1,49 @@
 import React from "react";
 import RoomFilter from "./RoomFilter";
 import RoomList from "./RoomList";
+import { withRoomConsumer } from "../context";
+import Loading from "./Loading";
+
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <>
+      <RoomFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </>
+  );
+}
+
+export default withRoomConsumer(RoomContainer);
+
+/* import React from "react";
+import RoomFilter from "./RoomFilter";
+import RoomList from "./RoomList";
 import { RoomConsumer } from "../context";
 import Loading from "./Loading";
 
-export default function RoomsContainer() {
+/*export default function RoomsContainer() {
   return (
     <RoomConsumer>
       {value => {
-        console.log(value);
+        const { loading, sortedRooms, rooms } = value;
 
+        if (loading) {
+          return <Loading />;
+        }
         return (
           <div>
             Room Container
-            <RoomFilter />
-            <RoomList />
+            <RoomFilter rooms={rooms} />
+            <RoomList rooms={sortedRooms} />
           </div>
         );
       }}
     </RoomConsumer>
   );
 }
+ */
--- src/context.js
+++ src/context.js
@@ -8,7 +8,16 @@ class RoomProvider extends Component {
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
-    loading: true
+    loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
   };
 
   //getData
@@ -17,11 +26,16 @@ class RoomProvider extends Component {
     //this.getData
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
-      loading: false
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize
     });
   }
 
@@ -42,9 +56,32 @@ class RoomProvider extends Component {
     return room;
   };
 
+  handleChange = event => {
+    const target = event.target;
+    const value = event.type === "checkbox" ? target.checked : target.value;
+    const name = event.target.name;
+
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
+  };
+
+  filterRooms = () => {
+    console.log("hi");
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
@@ -52,5 +89,15 @@ class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return (
+      <RoomConsumer>
+        {value => <Component {...props} context={value} />}
+      </RoomConsumer>
+    );
+  };
+}
 
 export { RoomProvider, RoomConsumer, RoomContext };
